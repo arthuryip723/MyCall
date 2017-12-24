e00efdae334bfc3fd135df10f4aad93e240e83d3
Arthur Yip <arthuryip723@gmail.com>
2017-12-23 23:47:46 -0500

    set up react.

---
+++ frontend/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', function () {
+  ReactDOM.render(
+    <h1>Hello Call!</h1>,
+    document.getElementById('root')
+  );
+});
