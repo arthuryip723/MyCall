396152a6def819d44b58e78497c84ed22789dccd
Arthur Yip <arthuryip723@gmail.com>
2018-02-10 22:59:10 -0500

    added stub for babeform

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import BabeIndexContainer from './babe/babe_index_container';
 import BabeShowContainer from './babe/babe_show_container';
+import BabeFormContainer from './babe/babe_form_container';
 
 // TODO Need to add other routes here.
 const App = () => (
 	<Switch>
 		<Route exact path="/" component={BabeIndexContainer} />
 		<Route exact path="/babes" component={BabeIndexContainer} />
+		<Route exact path="/babes/new" component={BabeFormContainer} />
 		<Route exact path="/babes/:babeId" component={BabeShowContainer} />
 		<Route component={BabeIndexContainer} />
 	</Switch>
--- frontend/components/babe/babe_index.jsx
+++ frontend/components/babe/babe_index.jsx
@@ -26,6 +26,7 @@ class BabeIndex extends React.Component {
         <ul>
           {this.props.babes.map((babe, idx) => <li key={idx}><Link to={'/babes/' + babe.id}>{babe.name}</Link></li>)}
         </ul>
+        <Link to='/babes/new'>Create babe</Link>
       </div>
     );
   }
