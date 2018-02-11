bcb79a0b65c0e5d14dd62c329d094cc8ffa10435
Arthur Yip <arthuryip723@gmail.com>
2018-02-11 14:57:06 -0500

    trivial progress on babe form

--- frontend/components/babe/babe_form.jsx
+++ frontend/components/babe/babe_form.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 
 class BabeForm extends React.Component {
+
+  handleSubmit(e) {
+    e.preventDefault();
+    alert('submitting...');
+    // TODO Fill this function.
+    // Call the dispatch from prop.
+  }
+
   render() {
-    return (<div>Babe form</div>);
+    // TODO Fill the form
+    return (
+      <div>
+        <h3>New babe</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" name="name" value=""></input>
+          </label> 
+          <br/><br/>
+          <label>
+            Age:
+            <input type="text" name="age" value=""></input>
+          </label> 
+          <br/><br/>
+          <button>Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
